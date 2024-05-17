aafb0fcdcb4c4d83b3931d7c23f6e4317dff781e
Jonathan  Mai <jonathannmai@gmail.com>
2024-05-17 14:16:37 -0700

    upload files
    upload files

---
+++ output1.asm
@@ -0,0 +1,21 @@
+------PARSED WITH NO ERRORS -----------------------
+# Operator  Operand
+1   SIN         
+2   POPM    5000
+3   SIN         
+4   POPM    5001
+5   PUSHM   5000
+6   PUSHM   5001
+7   GRT    
+8   JUMP0     11
+9   PUSHM   5000
+10  SOUT   
+11  LABEL  
+12  PUSHM   5001
+13  SOUT   
+14  LABEL  
+
+Identifier    Memory Addr   Type
+a             5000          integer       
+b             5001          integer       
+--------- NO ERRORS --------
---
+++ output3.asm
@@ -0,0 +1,38 @@
+------PARSED WITH NO ERRORS -----------------------
+# Operator  Operand
+1   PUSHI      0
+2   POPM    5002
+3   PUSHI      0
+4   POPM    5000
+5   SIN         
+6   POPM    5001
+7   SIN         
+8   POPM    5003
+9   LABEL  
+10  PUSHM   5000
+11  PUSHM   5001
+12  LES    
+13  JUMP0     23
+14  PUSHM   5002
+15  PUSHM   5000
+16  A      
+17  POPM    5002
+18  PUSHM   5000
+19  PUSHI      1
+20  A      
+21  POPM    5000
+22  JUMP       9
+23  PUSHM   5002
+24  PUSHM   5003
+25  EQU    
+26  JUMP0     29
+27  PUSHM   5002
+28  SOUT   
+29  LABEL  
+
+Identifier    Memory Addr   Type
+i             5000          integer       
+max           5001          integer       
+sum           5002          integer       
+target        5003          integer       
+--------- NO ERRORS --------
